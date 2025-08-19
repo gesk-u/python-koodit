87282d9aae18615f99d15df7704e31485548e432
gesk-u <anastasiiatkach10@gmail.com>
2025-08-19 11:26:59 +0300

    First commit!

---
+++ Rectangle.py
@@ -0,0 +1,6 @@
+length = float(input("Length: "))
+width = float(input ("Width: "))
+P = 2 * (length + width)
+A = length * width
+
+print(f"Perimeter: {P}\nArea: {A}")
---
+++ circle.py
@@ -0,0 +1,7 @@
+import math
+
+R = float(input("Enter radius: "))
+A = math.pi * (R**2)
+
+print(f"Area = {A:.2f} ")
+
